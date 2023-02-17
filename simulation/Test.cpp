69a288aa324233e9f71cb1e50e980e08d8a53554
Willem <1121567132@qq.com>
2023-02-17 13:29:21 +0800

    init

--- simulation/Test.cpp
+++ simulation/Test.cpp
@@ -7,8 +7,17 @@ using namespace std;
 int main() {
 	vector<int> v(10);
 
-	for (auto a : v) {
-		cout << a << " ";
+	v.push_back(3);
+	vector<int> list[10];
+	
+	list[1].push_back(1);
+	list[1].push_back(2);
+	list[2].push_back(1);
+	list[3].push_back(1);
+	
+	for (int i=0; i<list[1].size(); i++)
+	{
+		cout << list[1][i] << endl;
 	}
 
 
